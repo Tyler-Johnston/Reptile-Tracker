5a398708c6db7371c1c02e9492ea5eef55cebd79
Tyler Johnston <tyler@Tylers-MacBook-Air.local>
2023-02-19 16:44:41 -0700

    created updateReptile method

--- src/controllers/reptiles_controller.ts
+++ src/controllers/reptiles_controller.ts
@@ -12,8 +12,6 @@ type CreateReptileBody = {
   sex: "m" | "f",
 }
 
-// TODO: 1) create reptile 2) update reptile 3) delete reptile 4) xxxxlist all reptiles 
-
 const createReptile = (client: PrismaClient): RequestHandler =>
   async (req, res) => {
     const {userId, species, name, sex} = req.body as CreateReptileBody;
@@ -37,13 +35,38 @@ const createReptile = (client: PrismaClient): RequestHandler =>
 
   const updateReptile = (client: PrismaClient): RequestHandler =>
    async (req, res) => {
+    const {userId, species, name, sex} = req.body as CreateReptileBody;
+    const {reptileId} = req.params
+    
+    const reptile = await client.reptile.findFirst({
+      where: {
+        id: parseInt(reptileId)
+      },
+    });
+
+    if (reptile === null) {
+      return res.status(404).json({ message: 'Reptile not found' });
+    }
+
+    const updatedReptile = await client.reptile.update({
+      where: {
+        id: parseInt(reptileId)
+      },
+      data: {
+        userId: userId ?? reptile.userId,
+        species: species ?? reptile.species,
+        name: name ?? reptile.name,
+        sex: sex ?? reptile.sex
+      },
+    });
+
+    res.json({updatedReptile});
 
    }
 
    const getAllReptiles = (client: PrismaClient): RequestHandler =>
    async (req, res) => {
      const data = await client.reptile.findMany();
-   
      res.json({data});
    }
 
@@ -51,7 +74,7 @@ const createReptile = (client: PrismaClient): RequestHandler =>
    async (req, res) => {
     const reptileId = req.body.reptileId;
 
-    const reptile = await client.feeding.findFirst({
+    const reptile = await client.reptile.findFirst({
       where: {
         id: reptileId,
       },
@@ -61,14 +84,11 @@ const createReptile = (client: PrismaClient): RequestHandler =>
       return res.status(404).json({ message: 'Reptile not found' });
     }
 
-    const deletedReptile = await client.feeding.delete({
+    const deletedReptile = await client.reptile.delete({
       where: {
         id: reptileId,
       },
     });
-
-    
-
     res.json({ deletedReptile });
 
    }
