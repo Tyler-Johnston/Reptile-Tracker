8f12bb97449d9b5595a476d89faf49489d8dfaa1
Tyler Johnston <tyler@Tylers-MacBook-Air.local>
2023-02-19 17:31:22 -0700

    reptile_controller should be fine (TODO: add authentication)

--- src/controllers/reptiles_controller.ts
+++ src/controllers/reptiles_controller.ts
@@ -12,12 +12,11 @@ type CreateReptileBody = {
   sex: "m" | "f",
 }
 
-// TODO: 1) create reptile 2) update reptile 3) delete reptile 4) xxxxlist all reptiles 
-
 const createReptile = (client: PrismaClient): RequestHandler =>
   async (req, res) => {
     const {userId, species, name, sex} = req.body as CreateReptileBody
-    if (species ==="ball_python" ||  species ==="king_snake" || species ==="corn_snake" || species ==="redtail_boa"){
+
+    if ((species === "ball_python" ||  species === "king_snake" || species === "corn_snake" || species === "redtail_boa") && (sex === "m" || sex === "f")) {
     const reptile = await client.reptile.create({
       data: {
         userId,
@@ -28,72 +27,70 @@ const createReptile = (client: PrismaClient): RequestHandler =>
     });
     res.json({ reptile});
   } else {
-    return res.status(400).json({ message: 'Reptile species must match one of the following: ball_python or king_snake or corn_snake or redtail_boa'});
+    return res.status(400).json({ message: "Reptile species or sex does not match required type"});
   }
   }
 
-  const updateReptile = (client: PrismaClient): RequestHandler =>
+const updateReptile = (client: PrismaClient): RequestHandler =>
    async (req, res) => {
     const {userId, species, name, sex} = req.body as CreateReptileBody;
     const {reptileId} = req.params
-    
-    const reptile = await client.reptile.findFirst({
-      where: {
-        id: parseInt(reptileId)
-      },
-    });
 
-    if (reptile === null) {
-      return res.status(404).json({ message: 'Reptile not found' });
+    if ((species === "ball_python" ||  species === "king_snake" || species === "corn_snake" || species === "redtail_boa" || species === undefined) && (sex === "m" || sex === "f" || sex === undefined)) {
+      const reptile = await client.reptile.findFirst({
+        where: {
+          id: parseInt(reptileId)
+        },
+      });
+
+      if (reptile === null) {
+        return res.status(404).json({ message: 'Reptile not found' });
+      }
+  
+      const updatedReptile = await client.reptile.update({
+        where: {
+          id: parseInt(reptileId)
+        },
+        data: {
+          userId: userId ?? reptile.userId,
+          species: species ?? reptile.species,
+          name: name ?? reptile.name,
+          sex: sex ?? reptile.sex
+        },
+      });
+      res.json({updatedReptile});
     }
-
-    const updatedReptile = await client.reptile.update({
-      where: {
-        id: parseInt(reptileId)
-      },
-      data: {
-        userId: userId ?? reptile.userId,
-        species: species ?? reptile.species,
-        name: name ?? reptile.name,
-        sex: sex ?? reptile.sex
-      },
-    });
-
-    res.json({updatedReptile});
-
+    else {
+      return res.status(400).json({ message: "Reptile species or sex does not match required type"});
+    }
    }
 
-   const getAllReptiles = (client: PrismaClient): RequestHandler =>
+const getAllReptiles = (client: PrismaClient): RequestHandler =>
    async (req, res) => {
      const data = await client.reptile.findMany();
-   
      res.json({data});
    }
 
-   const deleteReptile = (client: PrismaClient): RequestHandler =>
+const deleteReptile = (client: PrismaClient): RequestHandler =>
    async (req, res) => {
-    const reptileId = req.body.reptileId;
+    const {reptileId} = req.params
 
     const reptile = await client.reptile.findFirst({
       where: {
-        id: reptileId,
+        id: parseInt(reptileId),
       },
     });
 
     if (reptile === null) {
-      return res.status(404).json({ message: 'Reptile not found' });
+      return res.status(404).json({ message: "Reptile not found" });
     }
 
     const deletedReptile = await client.reptile.delete({
       where: {
-        id: reptileId,
+        id: parseInt(reptileId),
       },
     });
-
-    
-
     res.json({ deletedReptile });
-
    }
 
 export const reptilesController = controller(
@@ -102,6 +99,6 @@ export const reptilesController = controller(
     { path: "/", method: "post", endpointBuilder: createReptile, skipAuth: true },
     { path: "/update/:reptileId", method: "put", endpointBuilder: updateReptile },
     { path: "/all", method: "get", endpointBuilder: getAllReptiles, skipAuth: true },
-    { path: "/delete", method: "delete", endpointBuilder: deleteReptile, skipAuth: true }
+    { path: "/delete/:reptileId", method: "delete", endpointBuilder: deleteReptile, skipAuth: true }
   ]
 )
