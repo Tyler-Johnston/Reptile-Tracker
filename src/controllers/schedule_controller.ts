ffbdd14c42a4661cc0b232a43c6492765819886c
Tyler Johnston <tyler@Tylers-MacBook-Air.local>
2023-02-19 18:56:57 -0700

    schedules should be working?

--- src/controllers/schedule_controller.ts
+++ src/controllers/schedule_controller.ts
@@ -17,44 +17,65 @@ type CreateScheduleBody = {
   friday: boolean,
   saturday: boolean,
   sunday: boolean
-
 }
 
 //TODO: 1) create a schedule 2) list all schedules for reptiles 3) list all schedules for users
 
 const createSchedule = (client: PrismaClient): RequestHandler =>
   async (req, res) => {
+
     const {reptileId, userId, type, description, monday, tuesday, wednesday, thursday, friday, saturday, sunday} = req.body as CreateScheduleBody;
-    const schedule = await client.schedule.create({
-      data: {
-        reptileId,
-        userId,
-        type,
-        description,
-        monday,
-        tuesday, 
-        wednesday,
-        thursday,
-        friday,
-        saturday,
-        sunday
-      },
-    });
-    res.json(schedule)
+    if (type === "feed" || type === "record" || type === "clean") {
+
+      const schedule = await client.schedule.create({
+        data: {
+          reptileId,
+          userId,
+          type,
+          description,
+          monday,
+          tuesday, 
+          wednesday,
+          thursday,
+          friday,
+          saturday,
+          sunday
+        },
+      });
+      res.json({schedule});
+    } else {
+      return res.status(400).json({message: "Invalid schedule type"})
+    }
   }
 
-  const getAllSchedules = (client: PrismaClient): RequestHandler =>
+const getAllReptileSchedules = (client: PrismaClient): RequestHandler =>
    async (req, res) => {
-     const data = await client.schedule.findMany();
-   
+    const reptileId = req.params.reptileId;
+     const data = await client.schedule.findMany({
+      where: {
+        reptileId: parseInt(reptileId)
+      }
+     });
      res.json({data});
    }
 
+const getAllUserSchedules = (client: PrismaClient): RequestHandler =>
+   async (req, res) => {
+    const userId = req.params.userId;
+    console.log("userID: " + userId)
+     const data = await client.schedule.findMany({
+      where: {
+        userId: parseInt(userId)
+      }
+     });
+     res.json({data});
+   }
 
 export const scheduleController = controller(
   "schedule",
   [
     { path: "/", method: "post", endpointBuilder: createSchedule, skipAuth: true },
-    { path: "/", method: "get", endpointBuilder: getAllSchedules, skipAuth: true }
+    { path: "/user/:userId", method: "get", endpointBuilder: getAllUserSchedules, skipAuth: true },
+    { path: "/reptile/:reptileId", method: "get", endpointBuilder: getAllReptileSchedules, skipAuth: true }
   ]
 )
