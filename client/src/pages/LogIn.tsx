88fac2ac9d16e7b018ea35ebd50d2413785000a2
Tyler Johnston <tyler@Tylers-MacBook-Air.local>
2023-03-30 16:57:34 -0600

    changed it from token auth to session based

--- client/src/pages/LogIn.tsx
+++ client/src/pages/LogIn.tsx
@@ -5,24 +5,35 @@ import { useApi } from "../hooks/useApi";
 export const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const api = useApi();
-  const setToken = useContext(AuthContext);
 
   async function signIn() {
-    console.log("it clicked button in login");
     const body = {
       email,
       password
     }
-    // const resultBody = await api.post(`${import.meta.env.DATABASE_URL}/users`, body)
-    const resultBody = await api.post("http://localhost:8000/sessions", body)
+
+    const result = await fetch("http://localhost:8000/sessions", {
+      method: 'post',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      credentials: "include",
+      body: JSON.stringify(body)
+    });
 
   }
 
   async function getInfo() {
-    const me = await api.get("http://localhost:8000/users/me")
-    console.log(me);
+    
+    const result = await fetch("http://localhost:8000/users/me", {
+      method: 'get',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      credentials: "include"
+    });
 
+    console.log(result)
   }
 
   return (
