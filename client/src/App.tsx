77b32685d172cdf6f9ef9a15dd4413de15de0d73
Tyler Johnston <johnstontyler@protonmail.com>
2023-03-29 20:27:23 -0600

    began creating client/pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,23 +2,40 @@ import { useEffect, useState } from "react"
 import { createBrowserRouter, RouterProvider, useLocation } from "react-router-dom"
 import { ApiContext } from "./contexts/api"
 import { Api } from "./lib/api"
-// import { Home } from "./pages/Home"
-// import { Root } from "./pages/Root"
-// import { User } from "./pages/User"
+import { Home } from "./pages/Home"
+import { Root } from "./pages/Root"
+import { Dashboard } from "./pages/Dashboard"
+import { SignUp } from "./pages/SignUp"
+import { Login } from "./pages/Login"
+import { Reptile } from "./pages/Reptile"
+
 
 const router = createBrowserRouter([
   {
     path: '/',
-    // element: <Root />,
+    element: <Root />,
     children: [
       {
-        // path: 'users/:id',
-        // element: <User />,
+        path: '/',
+        element: <Home />
       },
       {
-        // path: '/',
-        // element: <Home />
+        path: 'dashboard',
+        element: <Dashboard />,
+      },
+      {
+        path: 'signup',
+        element: <SignUp />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+      },
+      {
+        path: 'reptile',
+        element: <Reptile />,
       }
+
     ]
   },
 ])
