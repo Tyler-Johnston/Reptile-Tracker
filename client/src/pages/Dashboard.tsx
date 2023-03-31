7b59719b86daa56319ea5efa976370e361394919
Tyler Johnston <johnstontyler@protonmail.com>
2023-03-30 20:59:13 -0600

    delete reptile works on dashboard

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,29 +1,31 @@
 // 1. I should see all of the schedules for my user for the day of the week it is (for example, if it is Monday then I should only see the schedules that have me doing something on Monday.)
-// 2. I should see a list of all my reptiles
 // 3. When selecting a reptile the app should navigate to the Reptile page
 // 5. I should be able to delete a reptile.
 // 6. I should be able to log out of my account
 
 
-import { useContext, useState } from "react";
+import { useEffect, useState } from "react";
+
+interface Reptile {
+  id: number,
+  species: string,
+  name: string,
+  sex: string,
+}
 
 export const Dashboard = () => {
   const [species, setSpecies] = useState("ball_python");
-  const [name, setName] = useState("");
+  const [name, setName] = useState("unnamed reptile");
   const [sex, setSex] = useState("m");
+  const [reptiles, setReptiles] = useState([]);
+
+  const body = {
+    species,
+    name,
+    sex
+  }
 
   async function createReptile() {
-    
-    const body = {
-      species,
-      name,
-      sex
-    }
-
-    console.log("species: " + body.species)
-    console.log("name: " + body.name)
-    console.log("sex: " + body.sex)
-
     const result = await fetch("http://localhost:8000/reptile", {
       method: 'post',
       headers: {
@@ -33,6 +35,32 @@ export const Dashboard = () => {
       body: JSON.stringify(body)
     });
   }
+
+  async function getAllReptiles() {
+    const result = await fetch("http://localhost:8000/reptile", {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include"
+    });
+    const data = await result.json();
+    setReptiles(data.reptiles);
+  }
+
+  async function deleteReptile(id: number) {
+    const result = await fetch(`http://localhost:8000/reptile/${id}`, {
+      method: "delete",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include"
+    });
+  }
+
+  useEffect(() => {
+    getAllReptiles();
+  }, [reptiles]);
 
   return (
     <div>
@@ -55,10 +83,21 @@ export const Dashboard = () => {
 
       <button type="button" onClick={createReptile}>Create Reptile</button>
 
-
       {/* View All Reptiles */}
-      
+      <div>
+        {reptiles.map((reptile: Reptile) => (
+          <div key={reptile.id}>
+            <h3>{reptile.name}</h3>
+            <p>Species: {reptile.species}</p>
+            <p>Sex: {reptile.sex}</p>
+            <p>Rep Id: {reptile.id}</p>
+            <button type="button">View Reptile Info</button>
+            <button type="button" onClick={() => deleteReptile(reptile.id)}>Delete Reptile</button>
+          </div>
+        ))}
+      </div>
 
+    
       
     </div>
   )
