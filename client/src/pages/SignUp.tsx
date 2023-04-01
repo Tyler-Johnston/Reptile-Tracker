9423032164d6195113fb6aa52fc31529b13436fe
Tyler Johnston <tyler@Tylers-MacBook-Air.local>
2023-03-31 20:44:58 -0600

    home page/sign up page redirects

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,15 +1,15 @@
-import { useContext, useState } from "react";
-import { AuthContext } from "../contexts/auth";
-import { useApi } from "../hooks/useApi";
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+
 
 export const SignUp = () => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   async function signUp() {
-    console.log("it clicked buton");
     const body = {
       firstName,
       lastName,
@@ -26,27 +26,36 @@ export const SignUp = () => {
       body: JSON.stringify(body)
     });
 
+    navigate("/dashboard")
+
   }
 
   return (
-    <form className="signup-form">
-      <label>
-        First Name
-        <input value={firstName} onChange={e => setFirstName(e.target.value)} type="text" />
-      </label>
-      <label>
-        Last Name
-        <input value={lastName} onChange={e => setLastName(e.target.value)} type="text" />
-      </label>
-      <label>
-        Email
-        <input value={email} onChange={e => setEmail(e.target.value)} type="email" />
-      </label>
-      <label>
-        Password
-        <input value={password} onChange={e => setPassword(e.target.value)} type="password" />
-      </label>
-      <button type="button" onClick={signUp}>Sign up</button>
-    </form>
+    <div>
+      <form className="signup-form">
+        <label>
+          First Name
+          <input value={firstName} onChange={e => setFirstName(e.target.value)} type="text" />
+        </label>
+        <label>
+          Last Name
+          <input value={lastName} onChange={e => setLastName(e.target.value)} type="text" />
+        </label>
+        <label>
+          Email
+          <input value={email} onChange={e => setEmail(e.target.value)} type="email" />
+        </label>
+        <label>
+          Password
+          <input value={password} onChange={e => setPassword(e.target.value)} type="password" />
+        </label>
+        <button type="button" onClick={signUp}>Sign up</button>
+      </form>
+
+      <button type="button" onClick={() => navigate("/login")}>Login</button>
+
+    </div>
+
+
   )
 }
