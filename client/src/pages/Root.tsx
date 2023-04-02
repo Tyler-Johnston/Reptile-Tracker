96072485188a661426f2a539d16d943fd7ef5557
Tyler Johnston <johnstontyler@protonmail.com>
2023-04-02 13:31:29 -0600

    fixed infinite request loop bug and ensured reptile page auto-reloads

--- client/src/pages/Root.tsx
+++ client/src/pages/Root.tsx
@@ -9,12 +9,6 @@ export const Root = () => {
 
   return (
     <>
-    
-    <div style={{display:"flex"}}> 
-      <button type="button" onClick={() => navigate("/login")} style={{ background: '#6c757d', color: 'white', padding: '0.5rem 1rem', borderRadius: '0.25rem', border: 'none', marginTop: '1rem' }}>Login</button>
-      <button type="button" onClick={() => navigate("/signup")} style={{ background: '#6c757d', color: 'white', padding: '0.5rem 1rem', borderRadius: '0.25rem', border: 'none', marginTop: '1rem' }}>Sign Up</button>
-      <button type="button" onClick={() => navigate("/")} style={{ background: '#6c757d', color: 'white', padding: '0.5rem 1rem', borderRadius: '0.25rem', border: 'none', marginTop: '1rem' }}>Home</button>
-    </div>
       <Outlet />
     </>
   )
