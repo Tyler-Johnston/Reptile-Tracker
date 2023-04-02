8ba1d439ae94f0becb58e435496ae51cf9e76429
Andrew Stevenson <andystevenson910@gmail.com>
2023-04-02 12:26:19 -0600

    need you to test

--- client/src/pages/LogIn.tsx
+++ client/src/pages/LogIn.tsx
@@ -25,16 +25,17 @@ export const Login = () => {
   }
 
   return (
-    <form className="signup-form">
-      <label>
-        Email
-        <input value={email} onChange={e => setEmail(e.target.value)} type="email" />
-      </label>
-      <label>
-        Password
-        <input value={password} onChange={e => setPassword(e.target.value)} type="password" />
-      </label>
-      <button type="button" onClick={signIn}>Sign in</button>
-    </form>
+    <form className="signup-form" style={{ backgroundColor: '#f2f2f2', padding: '20px', borderRadius: '4px', maxWidth: '400px', margin: '0 auto' }}>
+  <label style={{ display: 'block', fontSize: '20px', marginBottom: '10px' }}>
+    Email
+    <input value={email} onChange={e => setEmail(e.target.value)} type="email" style={{ fontSize: '16px', padding: '8px', borderRadius: '4px', border: 'none', marginBottom: '10px' }} />
+  </label>
+  <label style={{ display: 'block', fontSize: '20px', marginBottom: '10px' }}>
+    Password
+    <input value={password} onChange={e => setPassword(e.target.value)} type="password" style={{ fontSize: '16px', padding: '8px', borderRadius: '4px', border: 'none', marginBottom: '10px' }} />
+  </label>
+  <button type="button" onClick={signIn} style={{ fontSize: '18px', padding: '10px 20px', backgroundColor: '#008CBA', color: 'white', border: 'none', borderRadius: '4px', cursor: 'pointer' }}>Sign in</button>
+</form>
+
   )
 }
