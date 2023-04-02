cf2fef83e1c3ed16f3735ef85f4d74e998ddb3c2
Andrew Stevenson <andystevenson910@gmail.com>
2023-04-02 12:45:10 -0600

    main pages pretty

--- client/src/pages/Root.tsx
+++ client/src/pages/Root.tsx
@@ -1,10 +1,20 @@
-import { Outlet, useLocation, useMatch } from "react-router-dom"
+import {useNavigate, Outlet, useLocation, useMatch } from "react-router-dom"
+import { Home } from "./Home";
+
+
 
 export const Root = () => {
   let name = "Home";
+  const navigate = useNavigate()
 
   return (
     <>
+    
+    <div style={{display:"flex"}}> 
+      <button type="button" onClick={() => navigate("/login")} style={{ background: '#6c757d', color: 'white', padding: '0.5rem 1rem', borderRadius: '0.25rem', border: 'none', marginTop: '1rem' }}>Login</button>
+      <button type="button" onClick={() => navigate("/signup")} style={{ background: '#6c757d', color: 'white', padding: '0.5rem 1rem', borderRadius: '0.25rem', border: 'none', marginTop: '1rem' }}>Sign Up</button>
+      <button type="button" onClick={() => navigate("/")} style={{ background: '#6c757d', color: 'white', padding: '0.5rem 1rem', borderRadius: '0.25rem', border: 'none', marginTop: '1rem' }}>Home</button>
+    </div>
       <Outlet />
     </>
   )
