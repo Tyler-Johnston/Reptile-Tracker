76561fd5684a7f2b15af6f0712a7d0cdc6c631e6
Tyler Johnston <johnstontyler@protonmail.com>
2023-04-02 16:34:45 -0600

    changed LogIn.tsx to SignIn.tsx

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 
-export const Login = () => {
+export const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
