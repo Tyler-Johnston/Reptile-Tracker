e3eeb22740082bb84dea1c126a94db606cc23154
Tyler Johnston <johnstontyler@protonmail.com>
2023-04-02 18:21:27 -0600

    error page added

---
+++ client/src/pages/Error.tsx
@@ -0,0 +1,11 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export const Error = () => {
+
+  return (
+    <>
+    <p>error! the page you tried to access does not exist</p>
+    </>
+  );
+};
