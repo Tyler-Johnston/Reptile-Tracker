c4df02c10ace86560a9b8fbda01a12349cd98a8b
Tyler Johnston <johnstontyler@protonmail.com>
2023-04-02 20:25:10 -0600

    changed description on home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -27,10 +27,10 @@ export const Home = () => {
   <h1 style={{ fontSize: '36px', marginBottom: '20px' }}>Reptile Tracker</h1>
   <h3 style={{ fontSize: '24px', marginBottom: '20px' }}>This is an app to allow you to...</h3>
   <ul style={{ fontSize: '20px', marginBottom: '20px' }}>
-    <li>Create an individual reptile</li>
+    <li>Create a reptile</li>
     <li>Create a schedule for said reptile</li>
-    <li>Create a husbandry record for a reptile</li>
-    <li>Record what and when you fed a reptile</li>
+    <li>Create a husbandry record for said reptile</li>
+    <li>Record what you wish to feed the reptiles</li>
   </ul>
   <button type="button" style={{ fontSize: '18px', padding: '10px 20px', backgroundColor: '#4CAF50', color: 'white', border: 'none', borderRadius: '4px', marginRight: '10px' }} onClick={() => navigate('/login')}>Login</button>
   <button type="button" style={{ fontSize: '18px', padding: '10px 20px', backgroundColor: '#008CBA', color: 'white', border: 'none', borderRadius: '4px' }} onClick={() => navigate('/signup')}>Sign Up</button>
